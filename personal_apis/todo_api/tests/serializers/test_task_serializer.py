aa4542c67a7d4d2f7067a688ec5d98563a0c9f60
Chathan Driehuys <cdriehuys@gmail.com>
2016-11-25 20:15:05 -0500

    Add task serializer.

---
+++ personal_apis/todo_api/tests/serializers/test_task_serializer.py
@@ -0,0 +1,50 @@
+from todo_api.models import Task
+from todo_api.serializers import TaskSerializer
+
+
+def test_deserialize(user_factory):
+    """
+    Test deserializing a task.
+
+    Given valid data, a task instance should be able to be constructed.
+    """
+    user = user_factory()
+    data = {
+        'user': user.pk,
+        'title': 'Test Task',
+        'description': 'Test task description.',
+        'done': False,
+    }
+
+    serializer = TaskSerializer(data=data)
+
+    assert serializer.is_valid()
+
+    task = serializer.save()
+
+    assert Task.objects.count() == 1
+    assert task.user == user
+    assert task.title == data['title']
+    assert task.description == data['description']
+    assert task.done == data['done']
+
+
+def test_serialize(task_factory):
+    """
+    Test serializing a task.
+
+    Serializing a task should return its JSON representation.
+    """
+    task = task_factory()
+
+    serializer = TaskSerializer(task)
+
+    expected = {
+        'id': task.id,
+        'user': task.user.pk,
+        'title': task.title,
+        'description': task.description,
+        'done': task.done,
+    }
+
+    assert serializer.data == expected
